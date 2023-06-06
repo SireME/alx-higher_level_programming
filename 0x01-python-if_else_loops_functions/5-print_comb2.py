0275fa03fb581e957f02a7bc84164360ca00cfb4
Mbang Ernest N <mbangernest@yahoo.com>
2023-06-06 22:44:03 +0100

    fix: reduce print statements

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,10 +3,7 @@
 # print from 0 t0 99 following specifics
 def o_to_99():
     for i in range(100):
-        if i != 99:
-            print("{:02d}, ".format(i), end="")
-        if i == 99:
-            print("{:02d}".format(i))
+        print("{:02d}".format(i), end=", " if i < 99 else "\n")
 
 
 o_to_99()
