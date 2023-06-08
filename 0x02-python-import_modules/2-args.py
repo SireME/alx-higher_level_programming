04f0260ea6094f7fb2b093bc9b4e5bb90b03b699
Mbang Ernest N <mbangernest@yahoo.com>
2023-06-09 00:02:06 +0100

    argument number

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    def print_arguements():
+        from sys import argv
+        ind = 1
+        alen = len(argv) - 1
+        arg_i = "argument" if alen == 1 else "arguments"
+
+        print("{:d} {} {}".format(alen, arg_i, "." if alen == 0 else ":"))
+        for i in argv:
+            if (i == argv[0]):
+                continue
+            print("{:d}: {}".format(ind, i))
+            ind += 1
+
+    print_arguements()
