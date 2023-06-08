9f2c3bc8ad97d4b046ab91573318a240b8b626ed
Mbang Ernest N <mbangernest@yahoo.com>
2023-06-09 00:07:17 +0100

    fix: remove extrace space in argument end

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
         alen = len(argv) - 1
         arg_i = "argument" if alen == 1 else "arguments"
 
-        print("{:d} {} {}".format(alen, arg_i, "." if alen == 0 else ":"))
+        print("{:d} {}{}".format(alen, arg_i, "." if alen == 0 else ":"))
         for i in argv:
             if (i == argv[0]):
                 continue
