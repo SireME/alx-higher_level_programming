81dcc0fbfa7c1ff3c52b2b56040209891bb86c2d
Mbang Ernest N <mbangernest@yahoo.com>
2023-06-08 22:32:55 +0100

    fix: initiate program only if main

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 
-
-def add_from_another():
-    import add_0
-
-    a = 1
-    b = 2
-    sum = add_0.add(a, b)
-    print("{:d} + {:d} = {:d}".format(a, b, sum))
-
-
 if __name__ == "__main__":
+    def add_from_another():
+        import add_0
+        a = 1
+        b = 2
+        sum = add_0.add(a, b)
+        print("{:d} + {:d} = {:d}".format(a, b, sum))
+
     add_from_another()
