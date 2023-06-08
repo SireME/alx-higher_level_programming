47ab8b89696f8bb8bbcd4ffb8ccf8ef1e8897598
Mbang Ernest N <mbangernest@yahoo.com>
2023-06-08 22:25:27 +0100

    fix: run program only if main

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
-# Import function
-import add_0
-
 
 def add_from_another():
+    import add_0
+
     a = 1
     b = 2
     sum = add_0.add(a, b)
     print("{:d} + {:d} = {:d}".format(a, b, sum))
 
 
-add_from_another()
+if __name__ == "__main__":
+    add_from_another()
