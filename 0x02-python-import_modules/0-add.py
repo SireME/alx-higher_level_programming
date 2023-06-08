95c97ae1c741036e8ef913af2268461267f31b55
Mbang Ernest N <mbangernest@yahoo.com>
2023-06-08 22:35:59 +0100

    fix: modify import style

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,10 +2,10 @@
 
 if __name__ == "__main__":
     def add_from_another():
-        import add_0
+        import from add_0 import add
         a = 1
         b = 2
-        sum = add_0.add(a, b)
+        sum = add(a, b)
         print("{:d} + {:d} = {:d}".format(a, b, sum))
 
     add_from_another()
