2be9cfd18857932dc0cc16bd450b15bdae55c694
Mbang Ernest N <mbangernest@yahoo.com>
2023-06-11 15:06:00 +0100

    multiple returns with tuple

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,15 @@
+"""
+Write a function that returns a tuple with the length of
+                                a string and its first character.
+
+Prototype: def multiple_returns(sentence):
+If the sentence is empty, the first character should be equal to None
+You are not allowed to import any module
+"""
+
+
+def multiple_returns(sentence):
+    length = len(sentence)
+    if length == 0:
+        return length, None
+    return length, sentence[0]
