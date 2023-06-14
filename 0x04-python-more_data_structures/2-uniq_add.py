0b1fe4fe15a30114fc310e5b23a4b48d13cdad78
Mbang Ernest N <mbangernest@yahoo.com>
2023-06-14 17:30:38 +0100

    add all unique integers

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Write a function that adds all unique integers
+                in a list (only once for each integer).
+
+Prototype: def uniq_add(my_list=[]):
+You are not allowed to import any module
+"""
+
+
+def uniq_add(my_list=[]):
+    return sum(set(my_list))
