48e1586d642c64503579091f0af6d637aafebc7a
Mbang Ernest N <mbangernest@yahoo.com>
2023-06-16 23:42:19 +0100

    fix: add exit status

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -4,8 +4,9 @@ import sys
 
 
 def syswrite(wr):
-    sys.stdout.write(wr)
+    sys.stderr.write(wr)
 
 
 text = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
 syswrite(text)
+sys.exit(1)
