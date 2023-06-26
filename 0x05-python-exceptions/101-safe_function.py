36deaee034a1483dd5c666ffbd11afb5e0461493
Mbang Ernest N <mbangernest@yahoo.com>
2023-06-26 16:41:21 +0100

    safely run function

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Write a function that executes a function safely.
+
+Prototype: def safe_function(fct, *args):
+You can assume fct will be always a pointer to a function
+Returns the result of the function,
+Otherwise, returns None if something happens
+             during the function and prints
+                  in stderr the error precede by Exception:
+You have to use try: / except:
+"""
+
+
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except Exception as es:
+        import sys
+        print("Exception: {}",format(es), file=sys.stderr)
+        return None
