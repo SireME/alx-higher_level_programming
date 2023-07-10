29ce08ed5ed7928f4d5741dc25faa9c03aba9b5d
Mbang Ernest N <mbangernest@yahoo.com>
2023-07-10 21:27:41 +0100

    empty class

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Write an empty class BaseGeometry.
+
+You are not allowed to import any module
+"""
+
+
+class BaseGeometry:
+    """
+    empty class that does nothing
+
+    Args:
+         None
+    Return:
+           None
+    """
+    pass
