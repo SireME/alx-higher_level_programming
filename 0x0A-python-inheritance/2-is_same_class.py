9e973e5e45637f57c6413e35a775dd86469c313b
Mbang Ernest N <mbangernest@yahoo.com>
2023-07-10 18:38:51 +0100

    fix: remove import from comments

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,7 +4,6 @@ Write a function that returns True if the object
 is exactly an instance of the specified class ; otherwise False.
 
 Prototype: def is_same_class(obj, a_class):
-You are not allowed to import any module
 """
 
 
