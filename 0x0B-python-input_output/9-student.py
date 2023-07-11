391886f53372f313aab541d134704486f20d43a1
Mbang Ernest N <mbangernest@yahoo.com>
2023-07-11 23:03:21 +0100

    create method within class for dictionary rep of class

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Write a class Student that defines a student by:
+
+Public instance attributes:
+first_name
+last_name
+age
+Instantiation with first_name, last_name and age:
+            def __init__(self, first_name, last_name, age):
+Public method def to_json(self): that retrieves a dictionary
+representation of a Student instance (same as 8-class_to_json.py)
+"""
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        return dictionary representation of class
+        Args:
+             None
+        Return:
+               dictionary of attributes and their corresponding vlaues
+        """
+        return self.__dict__
