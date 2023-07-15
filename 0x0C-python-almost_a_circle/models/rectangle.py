92728edc62366c17e728f2b96e4381b9e7b142cd
Mbang Ernest N <mbangernest@yahoo.com>
2023-07-15 15:26:09 +0100

    update class attributes using variadic function

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -81,6 +81,18 @@ class Rectangle(Base):
             print(" " * self.x, end="")
             print("#" * self.width + "\n", end="")
 
+    def update(self, *args):
+        """update instance attributes"""
+        self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) == 5:
+            self.y = args[4]
+
     def __str__(self):
         """overide str method with Rectangle parameters"""
         w = self.width
