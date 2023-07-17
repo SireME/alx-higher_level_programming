768ad80f6e3d28513e5c9391ee92461936484dee
Mbang Ernest N <mbangernest@yahoo.com>
2023-07-17 19:51:22 +0100

    add docs to init methods

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,6 +8,7 @@ from models.base import Base
 class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """ initialise rectangle attributes"""
         super().__init__(id)
         self.width = width
         self.height = height
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,6 +9,7 @@ class Square(Rectangle):
     """square class that inherites from rectangle"""
 
     def __init__(self, size, x=0, y=0, id=None):
+        """initialise square attributes"""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
