23539e0e9eadd9a04e093367c206db5ccb4e1265
Mbang Ernest N <mbangernest@yahoo.com>
2023-08-16 01:36:24 +0100

    list all records

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,8 @@
+-- list all records of table
+-- Records should be ordered by score
+-- The database name will be passed as
+-- an argument of the mysql command
+
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
