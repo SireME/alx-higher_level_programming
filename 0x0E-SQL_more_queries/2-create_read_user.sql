a5adb33b2a776b85daee8a564bdb78c505bca067
Mbang Ernest N <mbangernest@yahoo.com>
2023-08-16 22:18:48 +0100

    create user, table and grant privileges

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,14 @@
+-- Create database 
+
+CREATE database
+     IF NOT EXISTS hbtn_0d_2;
+
+-- create user on database
+CREATE USER 
+      IF NOT EXISTS user_0d_2@localhost
+	IDENTIFIED BY 'user_0d_2_pwd';
+
+-- set priviledge for user
+GRANT SELECT PRIVILEGES
+   ON 'hbtn_0d_2'
+   TO 'hbtn_0d_2'@localhost;
