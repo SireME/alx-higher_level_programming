51b710403c2069c8febc0382049067fc5e4a4979
Mbang Ernest N <mbangernest@yahoo.com>
2023-08-16 22:36:27 +0100

    fix query

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,14 +1,9 @@
 -- Create database 
 
-CREATE database
-     IF NOT EXISTS hbtn_0d_2;
+CREATE database IF NOT EXISTS hbtn_0d_2;
 
 -- create user on database
-CREATE USER 
-      IF NOT EXISTS user_0d_2@localhost
-	IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
 -- set priviledge for user
-GRANT SELECT PRIVILEGES
-   ON 'hbtn_0d_2'
-   TO 'hbtn_0d_2'@localhost;
+GRANT SELECT ON hbtn_0d_2.* TO 'hbtn_0d_2'@'localhost';
