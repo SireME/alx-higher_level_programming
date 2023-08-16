756a443b4345123408f092d32baf5cd9b6ef73a1
Mbang Ernest N <mbangernest@yahoo.com>
2023-08-16 18:20:56 +0100

    show privviledges for users

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- This query lists all priviledges of a sql user:
+-- user_0d_1 and user_0d_2 on your server (in localhost)
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
