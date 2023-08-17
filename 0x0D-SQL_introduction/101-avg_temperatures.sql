61f9d4287731a10056dfd8ad40ae257fcf217a02
Mbang Ernest N <mbangernest@yahoo.com>
2023-08-17 18:51:06 +0100

    simple computings

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,9 @@
+-- Write a script that displays the average
+-- temperature (Fahrenheit) by city
+-- ordered by temperature (descending).
+-- db: https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/272/temperatures.sql
+
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC
