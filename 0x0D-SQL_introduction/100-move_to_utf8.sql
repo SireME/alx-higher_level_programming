ecd72017bb54bf4eca05c902e263594283fdd42c
Mbang Ernest N <mbangernest@yahoo.com>
2023-08-17 19:53:14 +0100

    fix: to utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -5,11 +5,11 @@
 -- Table first_table
 -- Field name in first_table
 
--- Convert the database hbtn_0c_0 to UTF8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
 -- Switch to the hbtn_0c_0 database
 USE hbtn_0c_0;
+
+-- Convert the database hbtn_0c_0 to UTF8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- Convert the table first_table to UTF8
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
