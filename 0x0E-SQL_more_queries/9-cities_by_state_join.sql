5ff5030b467e1ca754694070b0d1ee9a7e4d7a74
Mbang Ernest N <mbangernest@yahoo.com>
2023-08-17 12:52:24 +0100

    inner join demonstration

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,9 @@
+-- Write a script that lists all cities contained
+-- in the database hbtn_0d_usa
+-- dEach record should display: cities.id -
+-- cities.name - states.name ++
+
+SELECT cities.id, cities.name, states.name
+FROM cities JOIN states
+     ON cities.state_id = states.id
+ORDER BY cities.id
