4cee7e03ebef5c31d8a8016cb0615da101722037
Mbang Ernest N <mbangernest@yahoo.com>
2023-09-12 21:33:38 +0100

    initialise empty class constructor with height and width

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// initialise empty class and initialise constructor
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
