98cd042629ab81a61d274baf6b5c8238b85fed50
Mbang Ernest N <mbangernest@yahoo.com>
2023-09-12 21:51:27 +0100

    set constructor conditionally

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// initialise empty class and initialise
+// constructor except when values are invalid
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle;
