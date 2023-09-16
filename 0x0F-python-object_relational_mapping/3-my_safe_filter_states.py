be8343a60b4853410db5e47da2eed90152aa6b29
Mbang Ernest N <mbangernest@yahoo.com>
2023-09-16 17:21:22 +0100

    select specific stae sql injection safe

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+this script  lists all states from the database hbtn_0e_0_usa
+"""
+
+
+if __name__ == "__main__":
+    def list_states_by_name(un, pwd, db_nm, state_name):
+        """
+        get all row with specific input state, takes in state
+        as 4th argument sql injection safe
+        note: table created from .sql script of similar name
+        """
+        # keyword arguments for connect
+        karg = {"host": "localhost", "user": un, "password": pwd}
+        karg["database"] = db_nm
+        karg["port"] = 3306
+        karg["charset"] = "utf8"
+
+        # connection to db
+        db = MySQLdb.connect(**karg)
+
+        # cursor creation
+        cc = db.cursor()
+
+        # command execution
+        sn = state_name
+        cc.execute("""SELECT * FROM states WHERE name = %s\
+                ORDER By id ASC;""",(sn,))
+        results = cc.fetchall()
+        # display of exectution result
+        for row in results:
+            if row[1] == argv[4]:
+                print(row)
+
+        # close cursor and database
+        cc.close()
+        db.close()
+
+    # execute code list all states in db
+    import MySQLdb
+    from sys import argv
+    list_states_by_name(argv[1], argv[2], argv[3], argv[4])
