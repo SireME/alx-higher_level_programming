7a4b8a8d1c8be1250830df92cab1c5dac3ddbbea
Mbang Ernest N <mbangernest@yahoo.com>
2023-09-16 15:15:16 +0100

    list all states following pattern

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+this script  lists all states from the database hbtn_0e_0_usa
+with name starting with N
+"""
+
+
+if __name__ == "__main__":
+    def list_all_states(un, pwd, db_nm):
+        """
+        get all rows from specific table starting with N
+        note: table created from .sql script of similar name
+        """
+        # keyword arguments for connect
+        karg = {"host": "localhost", "user": un, "password": pwd}
+        karg["database"] = db_nm
+        karg["port"] = 3306
+        karg["charset"] = "utf8"
+
+        # connection to db
+        db = MySQLdb.connect(**karg)
+
+        # cursor creation
+        cc = db.cursor()
+
+        # command execution
+        cc.execute("""
+        SELECT *
+        FROM states
+        WHERE states.name LIKE 'N%'
+        ORDER By states.id;
+        """)
+
+        # display of exectution result
+        for row in cc.fetchall():
+            print(row)
+
+        # close cursor and database
+        cc.close()
+        db.close()
+
+    # execute code list all states in db
+    import MySQLdb
+    from sys import argv
+    list_all_states(argv[1], argv[2], argv[3])
