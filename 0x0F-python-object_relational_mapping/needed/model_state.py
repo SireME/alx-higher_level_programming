8964b3378917b4e535002b85627d63260fda1a12
Mbang Ernest N <mbangernest@yahoo.com>
2023-09-17 10:53:48 +0100

    sqlalchemy create table class

--- 0x0F-python-object_relational_mapping/needed/model_state.py
+++ 0x0F-python-object_relational_mapping/needed/model_state.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
 """
-this file contains code to create db
-table class
+create db class
 """
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv as av
+
+
+Base = declarative_base()
+db = f"mysql+mysqldb://{av[1]}:{av[2]}@127.0.0.1:3306/{av[3]}"
+engine = create_engine(db)
 
 
 class State(Base):
     """
-    state class that maps to
-    sqlalchemy table
+    state class that maps to table
     """
     __tablename__ = "state"
     id = Column(Integer, primary_key=True)
