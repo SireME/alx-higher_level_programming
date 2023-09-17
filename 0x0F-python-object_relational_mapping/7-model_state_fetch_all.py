2f0816b4c35e9dac455ed43625acecf706e4f6b0
Mbang Ernest N <mbangernest@yahoo.com>
2023-09-17 11:52:03 +0100

    query and format database

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+create db class
+"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv as av
+
+
+if __name__ == "__main__":
+    db = f"mysql+mysqldb://{av[1]}:{av[2]}@127.0.0.1:3306/{av[3]}"
+    engine = create_engine(db)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    result = session.query(State).order_by(State.id).all()
+    for instance in result:
+        print(f'{instance.id}: {instance.name}')
