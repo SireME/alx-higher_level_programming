53cf15bea14dbe417338ce1d3e248f175aac5027
Mbang Ernest N <mbangernest@yahoo.com>
2023-09-29 02:47:34 +0100

    make script executable

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends a request to url, display the size of the body of the response
-curl -s &1 |grep -i Content-length | Awk '{print $2}'
+curl -sI &1 |grep -i Content-length | Awk '{print $2}'
