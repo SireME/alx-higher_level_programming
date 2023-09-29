d77370e0339da0b6e5eb319792403b15907e0e47
Mbang Ernest N <mbangernest@yahoo.com>
2023-09-29 03:27:58 +0100

    send and get response with header value

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# dispaly HTTP methods server will accept
-curl -sL $1 | grep Allow | cut -d " " -f 2-
+# send get request and display response body send along headerwith value as seen
+curl -sH "X-School-User-Id: 98" "$1"
