51759749d56fe91961c4c282daeb669b679f57bf
Mbang Ernest N <mbangernest@yahoo.com>
2023-09-29 02:45:50 +0100

    modify length of script

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Write a Bash script that takes in a URL,
-#sends a request to that URL, and displays
-#the size of the body of the response
+#sends a request to url, display the size of the body of the response
 curl -s &1 |grep -i Content-length | Awk '{print $2}'
