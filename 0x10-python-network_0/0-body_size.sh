86a1bc86d918165c206f40e417f0e5c0da147be9
Mbang Ernest N <mbangernest@yahoo.com>
2023-09-29 03:00:11 +0100

    modify command syntax

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends a request to url, display the size of the body of the response
-curl -sI &1 | grep Content-Length | awk '{print $2}'
+curl -sI $1 | grep -i "content-length" | cut -d " " -f2
