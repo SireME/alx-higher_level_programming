5184fdfe8fc18cfa21e625f11782f056eaa27e7b
Mbang Ernest N <mbangernest@yahoo.com>
2023-09-30 03:14:39 +0100

    fetch content at url

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Script that fetches the content of the URL https://intranet.hbtn.io/status
+with response package
+"""
+import requests
+
+
+if __name__ == '__main__':
+    url_to_fetch = "https://intranet.hbtn.io/status"
+    response = requests.get(url_to_fetch)
+    response_text = response.text
+    print("Body response:")
+    print("\t- Type: {}".format(type(response_text)))
+    print("\t- Content: {}".format(response_text))
