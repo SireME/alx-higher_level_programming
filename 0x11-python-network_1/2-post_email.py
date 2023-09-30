127c230d05ce49cbe0a4ac02c92487890714fba8
Mbang Ernest N <mbangernest@yahoo.com>
2023-09-30 02:57:59 +0100

    modify script params

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
     dic = {"email": sys.argv[2]}
     data = urllib.parse.urlencode(dic).encode("ascii")
-
     request = urllib.request.Request(url, data)
+
     with urllib.request.urlopen(request) as response:
         print(response.read().decode("utf-8"))
