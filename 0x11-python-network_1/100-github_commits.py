e8af183e5cded49f016de9942e35bef12f0b8796
Mbang Ernest N <mbangernest@yahoo.com>
2023-10-03 10:07:00 +0100

    use github api to print commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -3,15 +3,17 @@
 This script collects the first 10 commits from a
 specific repo with its author
 """
-from requests import get
-from sys import argv
 
-url = f"https://api.github.com/repos/{argv[1]}/{argv[2]}/commits"
-r = get(url).json()
+if __name__ == "__main__":
+    from requests import get
+    from sys import argv
 
-for i in range(10):
-    result = ""
-    result += r[i]["sha"]
-    result += ": "
-    result += r[i]["commit"]["author"]["name"]
-    print(result)
+    url = f"https://api.github.com/repos/{argv[1]}/{argv[2]}/commits"
+    r = get(url).json()
+
+    for i in range(10):
+        result = ""
+        result += r[i]["sha"]
+        result += ": "
+        result += r[i]["commit"]["author"]["name"]
+        print(result)
