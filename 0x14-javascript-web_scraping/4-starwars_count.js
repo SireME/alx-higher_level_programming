917b49188435b91d44618f183a38c2315dd0c6ad
Mbang Ernest N <mbangernest@yahoo.com>
2023-10-24 12:32:05 +0100

    get movies using only films url

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,27 +2,20 @@
 
 const request = require('request');
 const apiUrl = process.argv[2];
-let character;
+let present = 0;
 
 // query number of movies by character Wedge Antilles
 request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else if (!error && response.statusCode === 200) {
+  if (!error && response.statusCode === 200) {
     const jsonObject = JSON.parse(body);
-    for (const url of jsonObject.results[0].characters) {
-      if (url === 'https://swapi-api.alx-tools.com/api/people/18/') {
-        character = url;
+    for (const film of jsonObject.results) {
+      for (const url of film.characters) {
+        if (url === 'https://swapi-api.alx-tools.com/api/people/18/') {
+          present++;
+        }
       }
     }
+    console.log(present);
   }
-
-  request(character, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-      const numberFilm = JSON.parse(body).films.length;
-      console.log(numberFilm);
-    }
-  }
-  );
 }
 );
