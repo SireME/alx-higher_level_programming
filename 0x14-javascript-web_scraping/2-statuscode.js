d5549921f58741047627b7d389956722a0340a00
Mbang Ernest N <mbangernest@yahoo.com>
2023-10-24 10:35:26 +0100

    fix status code capture

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,10 +5,9 @@ const url = process.argv[2];
 
 // readding and printing content od a file
 request(url, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    console.log('code: 200');
-  } else {
-    console.log('code: 404');
+  if (error) {
+    console.log(error);
   }
+  console.log(`code: ${response.statusCode}`);
 }
 );
