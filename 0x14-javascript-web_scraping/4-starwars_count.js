2ad08625e7dfdc5c6e2414679a8c612b90913cc6
Mbang Ernest N <mbangernest@yahoo.com>
2023-10-24 12:37:10 +0100

    add error handling

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,16 +6,19 @@ let present = 0;
 
 // query number of movies by character Wedge Antilles
 request(apiUrl, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    const jsonObject = JSON.parse(body);
-    for (const film of jsonObject.results) {
-      for (const url of film.characters) {
-        if (url === 'https://swapi-api.alx-tools.com/api/people/18/') {
-          present++;
+  if (error) {
+    console.log(error);
+  } else
+    if (!error && response.statusCode === 200) {
+      const jsonObject = JSON.parse(body);
+      for (const film of jsonObject.results) {
+        for (const url of film.characters) {
+          if (url === 'https://swapi-api.alx-tools.com/api/people/18/') {
+            present++;
+          }
         }
       }
+      console.log(present);
     }
-    console.log(present);
-  }
 }
 );
