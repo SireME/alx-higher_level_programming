2697912c1d353203820aec1882536c91fbfe2ea9
Mbang Ernest N <mbangernest@yahoo.com>
2023-10-24 12:13:05 +0100

    fix add explicit error handling

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,7 +6,9 @@ let character;
 
 // query number of movies by character Wedge Antilles
 request(apiUrl, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
+  if (error) {
+    console.log(error);
+  } else if (!error && response.statusCode === 200) {
     const jsonObject = JSON.parse(body);
     for (const url of jsonObject.results[0].characters) {
       if (url === 'https://swapi-api.alx-tools.com/api/people/18/') {
