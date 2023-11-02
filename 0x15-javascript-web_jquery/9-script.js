06b43a0e6f2a4ddb9f0c92d599b2df5a19cb8e77
Mbang Ernest N <mbangernest@yahoo.com>
2023-11-02 10:38:47 +0100

    fetch and populate frontend with data

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,15 @@
+/*
+ Write a JavaScript script that fetches from https://hellosalut.stefanbohacek.dev/?lang=fr and displays the value of hello from that fetch in the HTML tag DIV#hello.
+
+The translation of “hello” must be displayed in the HTML tag DIV#hello
+You can’t use document.querySelector to select the HTML tag
+You must use the JQuery API
+Your script must work when it is imported from the <head> tag
+ */
+
+$(() => {
+  const url = 'https://hellosalut.stefanbohacek.dev/?lang=fr';
+  $.get(url, (data) => {
+    $('DIV#hello').text(data.hello);
+  });
+});
